38caf41ae832cbafe584ef02ee623e16de773e47
rivopelu <rivopelu12@gmail.com>
2025-03-07 00:00:27 +0700

    FEAT:setup i18n

--- src/main/java/com/warehouse/app/services/impl/AuthServiceImpl.java
+++ src/main/java/com/warehouse/app/services/impl/AuthServiceImpl.java
@@ -2,12 +2,17 @@ package com.warehouse.app.services.impl;
 
 import com.warehouse.app.dto.request.RequestCreateAccount;
 import com.warehouse.app.services.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.warehouse.app.utilities.UtilsHelper.getMessage;
+
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
     @Override
     public String register(RequestCreateAccount requestCreateAccount) {
-        return "success";
+        return getMessage("success");
     }
 }
--- src/main/java/com/warehouse/app/utilities/UtilsHelper.java
+++ src/main/java/com/warehouse/app/utilities/UtilsHelper.java
@@ -1,8 +1,27 @@
 package com.warehouse.app.utilities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
 public class UtilsHelper {
+
+    private static MessageSource messageSource;
+
+    @Autowired
+    public UtilsHelper(MessageSource messageSource) {
+        UtilsHelper.messageSource = messageSource;
+    }
+
     public static String generateAvatar(String name) {
         return "https://ui-avatars.com/api/?name=" + name;
     }
 
+
+    public static String getMessage(String key) {
+        return messageSource.getMessage(key, null, Locale.ENGLISH);
+    }
 }
