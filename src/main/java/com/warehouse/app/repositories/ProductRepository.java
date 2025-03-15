f312fdb9c7385a541121fb2563bc693ba5e6c2f7
rivo pelu <daeveon.davin@thefluent.org>
2025-03-15 21:11:18 +0700

    FEAT:setup

--- src/main/java/com/warehouse/app/repositories/ProductRepository.java
+++ src/main/java/com/warehouse/app/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.warehouse.app.repositories;
 
 import com.warehouse.app.entities.Product;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
