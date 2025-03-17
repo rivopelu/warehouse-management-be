0a862c2ccf91eecb314a41c88eda218f534171eb
rivo pelu <daeveon.davin@thefluent.org>
2025-03-17 20:35:12 +0700

    FEAT:running migration 1.4.9

---
+++ src/main/java/com/warehouse/app/entities/Stock.java
@@ -0,0 +1,29 @@
+package com.warehouse.app.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "stock")
+public class Stock extends BaseEntity {
+    @JoinColumn(name = "warehouse_id")
+    @ManyToOne
+    private Warehouse warehouse;
+
+    @JoinColumn(name = "unit_type_id")
+    @ManyToOne
+    private UnitType unitType;
+
+    @JoinColumn(name = "variant_product_id")
+    @ManyToOne
+    private VariantProduct variantProduct;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+}
